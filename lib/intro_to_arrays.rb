766f5a40a0b50766d106c94819b43c89c4bd4c47
Jess Rezac <jess.rezac@gmail.com>
2019-04-20 03:23:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,3 +7,24 @@ def array_with_two_elements
   new_array = ["first element", "second element"]
   return new_array
 end
+
+def indexing_array
+  new_array = array_with_two_elements
+  new_array.push("last_element")
+  return new_array
+end
+
+def first_element
+  new_array = indexing_array
+  return new_array[0]
+end
+
+def second_element
+  new_array = indexing_array
+  return new_array[1]
+end
+
+def last_element
+  new_array = indexing_array
+  return new_array[-1]
+end
