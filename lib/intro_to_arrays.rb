c3919ecacece6bcd343aec8452a420bd5399d028
Jess Rezac <jess.rezac@gmail.com>
2019-04-20 03:24:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,30 +1,29 @@
 def instantiate_new_array
   new_array = []
-  return new_array
+  new_array
 end
 
 def array_with_two_elements
   new_array = ["first element", "second element"]
-  return new_array
+  new_array
 end
 
-def indexing_array
-  new_array = array_with_two_elements
-  new_array.push("last_element")
-  return new_array
+def first_element(array)
+ array[0]
 end
 
-def first_element
-  new_array = indexing_array
-  return new_array[0]
+def second_element(array)
+ array[1]
 end
 
-def second_element
-  new_array = indexing_array
-  return new_array[1]
+def last_element(array)
+ array[-1]
 end
 
-def last_element
-  new_array = indexing_array
-  return new_array[-1]
+def first_element_with_array_methods(array)
+ array.shift
+end
+
+def last_element_with_array_methods(array)
+  array.pop
 end
